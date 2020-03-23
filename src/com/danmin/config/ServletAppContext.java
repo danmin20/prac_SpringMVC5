2173c100822564556b95582c30d1f8ed0462ebdf
leejeongmin <danmin20@khu.ac.kr>
2020-03-23 20:44:29 +0900

    mybatis setting

--- src/com/danmin/config/ServletAppContext.java
+++ src/com/danmin/config/ServletAppContext.java
@@ -1,16 +1,39 @@
 package com.danmin.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.danmin.mapper.BoardMapper;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.danmin.controller")
+@PropertySource("/WEB-INF/properties/db.properties")
 public class ServletAppContext implements WebMvcConfigurer {
+
+	@Value("${db.classname}")
+	private String db_classname;
+
+	@Value("${db.url}")
+	private String db_url;
+
+	@Value("${db.username}")
+	private String db_username;
+
+	@Value("${db.password}")
+	private String db_password;
+
 	// Controller의 메소드가 반환하는 jsp의 이름 앞뒤에 경로와 확장자를 붙여줌
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
@@ -23,5 +46,36 @@ public class ServletAppContext implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		WebMvcConfigurer.super.addResourceHandlers(registry);
 		registry.addResourceHandler("/**").addResourceLocations("/resources/");
+	}
+
+	// 데이터베이스 접속 정보를 관리하는 Bean
+	@Bean
+	public BasicDataSource dataSource() {
+		BasicDataSource source = new BasicDataSource();
+		source.setDriverClassName(db_classname);
+		source.setUrl(db_url);
+		source.setUsername(db_username);
+		source.setPassword(db_password);
+
+		return source;
+	}
+
+	// 쿼리문과 접속정보를 관리하는 객체
+	@Bean
+	public SqlSessionFactory factory(BasicDataSource source) throws Exception {
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(source);
+		SqlSessionFactory factory = factoryBean.getObject();
+
+		return factory;
+	}
+
+	// 쿼리문 실행을 위한 객체
+	@Bean
+	public MapperFactoryBean<BoardMapper> getBoardMapper(SqlSessionFactory factory) throws Exception {
+		MapperFactoryBean<BoardMapper> factoryBean = new MapperFactoryBean<BoardMapper>(BoardMapper.class);
+		factoryBean.setSqlSessionFactory(factory);
+
+		return factoryBean;
 	}
 }
---
+++ src/com/danmin/mapper/BoardMapper.java
@@ -0,0 +1,5 @@
+package com.danmin.mapper;
+
+public interface BoardMapper {
+
+}
