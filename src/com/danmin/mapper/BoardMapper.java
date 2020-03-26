36bf2151c7e857c29e42d57d5f4021ce02f0ba8e
leejeongmin <danmin20@khu.ac.kr>
2020-03-26 21:17:19 +0900

    implement modify

--- src/com/danmin/mapper/BoardMapper.java
+++ src/com/danmin/mapper/BoardMapper.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectKey;
+import org.apache.ibatis.annotations.Update;
 
 import com.danmin.beans.ContentBean;
 
@@ -30,7 +31,12 @@ public interface BoardMapper {
 
 	@Select("select a2.user_name as content_writer_name, "
 			+ "    to_char(a1.content_date, 'YYYY-MM-DD') as content_date, "
-			+ "    a1.content_subject, a1.content_text, a1.content_file, a1.content_writer_idx " + "from content_table a1, user_table a2 "
-			+ "where a1.content_writer_idx = a2.user_idx " + "    and content_idx = #{content_idx}")
+			+ "    a1.content_subject, a1.content_text, a1.content_file, a1.content_writer_idx "
+			+ "from content_table a1, user_table a2 " + "where a1.content_writer_idx = a2.user_idx "
+			+ "    and content_idx = #{content_idx}")
 	ContentBean getContentInfo(int content_idx);
+
+	@Update("update content_table " + "set content_subject = #{content_subject}, content_text = #{content_text}, "
+			+ "content_file = #{content_file, jdbcType=VARCHAR} " + "where content_idx = #{content_idx}")
+	void modifyContentInfo(ContentBean modifyContentBean);
 }
